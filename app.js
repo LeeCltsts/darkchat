6e3a7371647d251131c4be085875ba4bab404864
LeeCltsts <150625259+LeeCltsts@users.noreply.github.com>
2023-11-12 13:54:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,240 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const rooms = {};
+const connectedUsers = [];
+const searchingUsers = [];
+const chatSessions = {};
+const lastKnownSizes = {};
 
 app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello, world!',
-  })
-})
+    res.send('running');
+});
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+io.on('connection', (socket) => {
+    socket.on('message', (msg) => {
+        console.log('Message:', msg);
+        io.emit('message', msg);
+    });
+});
+
+function eventStream(res, room_id) {
+    const intervalId = setInterval(() => {
+        const messages = rooms[room_id] || [];
+        const currentSize = messages.length;
+
+        if (currentSize > (lastKnownSizes[room_id] || 0)) {
+            lastKnownSizes[room_id] = currentSize;
+            res.write(`data: ${JSON.stringify({ messages })}\n\n`);
+        }
+    }, 1000);
+
+    res.on('close', () => {
+        clearInterval(intervalId);
+    });
+}
+
+app.get('/get_messages_sse', (req, res) => {
+    const room_id = parseInt(req.query.room_id);
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    });
+    eventStream(res, room_id);
+});
+
+function strangerEventStream(res, user_id) {
+    const intervalId = setInterval(() => {
+        if (user_id in chatSessions) {
+            const stranger_id = chatSessions[user_id]['id'];
+            const messages = chatSessions[stranger_id]['messages'];
+            res.write(`data: ${JSON.stringify({ messages })}\n\n`);
+        }
+    }, 1000);
+
+    res.on('close', () => {
+        clearInterval(intervalId);
+    });
+}
+
+app.get('/get_stranger_messages_sse', (req, res) => {
+    const user_id = req.query.user_id;
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    });
+    strangerEventStream(res, user_id);
+});
+
+app.get('/get_messages', (req, res) => {
+    const room_id = parseInt(req.query.room_id);
+    const messages = rooms[room_id] || [];
+    res.json({ messages, room_id });
+});
+
+app.get('/get_stranger_messages', (req, res) => {
+    const user_id = req.query.user_id;
+    const stranger_id = req.query.stranger_id;
+    if (chatSessions[user_id] && chatSessions[user_id]['id'] === stranger_id) {
+        const messages = chatSessions[stranger_id]['messages'];
+        res.json({ messages });
+    } else {
+        res.status(404).json({ error: 'User not found or not in a chat session' });
+    }
+});
+
+app.get('/fetch_rooms', (req, res) => {
+    res.json({ rooms: Object.keys(rooms) });
+});
+
+app.post('/create_room', (req, res) => {
+    const room_id = Object.keys(rooms).length + 1;
+    rooms[room_id] = [];
+    res.json({ roomId: room_id });
+});
+
+app.post('/send_message', (req, res) => {
+    const { message, msg_id, user_id } = req.body;
+    const room_id = parseInt(req.query.room_id);
+
+    if (rooms[room_id]) {
+        rooms[room_id].push({ message, msg_id, user_id });
+        res.json({ message, msg_id, user_id });
+    } else {
+        res.status(404).json({ error: 'Room not found' });
+    }
+});
+
+app.post('/send_stranger_message', (req, res) => {
+    const { message, msg_id, user_id, recipient_id } = req.body;
+
+    if (chatSessions[user_id] && chatSessions[user_id]['id'] === recipient_id) {
+        chatSessions[user_id]['messages'].push({ message, msg_id, user_id });
+        res.json({ message, msg_id, user_id });
+    } else {
+        res.status(404).json({ error: 'User not found or not in a chat session' });
+    }
+});
+
+app.all('/check_queue', (req, res) => {
+    if (req.method === 'POST') {
+        const data = req.body;
+
+        if (searchingUsers.length > 0) {
+            const user = {
+                id: data.user_id,
+                interests: new Set(data.interests),
+            };
+
+            const stranger = findStranger(user);
+
+            if (stranger) {
+                chatSessions[user.id] = { id: stranger, messages: [] };
+                getAndPopUserById(searchingUsers, stranger);
+                res.json({ message: stranger });
+                return;
+            }
+        }
+
+        res.json({ message: 'Match not found' });
+    } else {
+        res.json({ message: 'Invalid method' });
+    }
+});
+
+app.post('/connect', (req, res) => {
+    try {
+        const data = req.body;
+
+        if (!data.user_id || !data.interests) {
+            res.status(400).json({ error: 'Invalid request data' });
+            return;
+        }
+
+        const user = {
+            id: data.user_id,
+            interests: new Set(data.interests),
+        };
+
+        connectedUsers.push(user);
+        searchingUsers.push(user);
+        res.json({ message: 'Connected to server' });
+    } catch (error) {
+        res.status(500).json({ error: error.toString() });
+    }
+});
+
+app.post('/disconnect', (req, res) => {
+    const user_id = req.body.id;
+
+    if (user_id in chatSessions) {
+        delete chatSessions[user_id];
+    }
+
+    const user = getAndPopUserById(connectedUsers, user_id);
+    getAndPopUserById(searchingUsers, user_id);
+
+    if (user) {
+        res.json({ message: 'Disconnected from server' });
+    } else {
+        res.json({ message: 'Not currently in a chat session or connected to server' });
+    }
+});
+
+app.get('/check_stranger_disconnect', (req, res) => {
+    const stranger_id = req.query.id;
+
+    if (!connectedUsers.some((user) => user.id === stranger_id)) {
+        getAndPopUserById(connectedUsers, stranger_id);
+        res.json({ disconnected: true });
+    } else {
+        res.json({ disconnected: false });
+    }
+});
+
+function findStranger(user) {
+    const matchingUsers = searchingUsers.filter(
+        (u) => u.id !== user.id && [...u.interests].some((interest) => user.interests.has(interest))
+    );
+
+    if (matchingUsers.length > 0) {
+        const stranger = matchingUsers[Math.floor(Math.random() * matchingUsers.length)];
+        return stranger.id;
+    } else {
+        return null;
+    }
+}
+
+function getAndPopUserById(userList, userId) {
+    const index = userList.findIndex((user) => user.id === userId);
+
+    if (index !== -1) {
+        const poppedUser = userList.splice(index, 1)[0];
+        return poppedUser.id;
+    }
+
+    return null;
+}
+
+function createId() {
+    const idLength = 26;
+    return uuidv4().replace(/-/g, '').substring(0, idLength);
+}
+
+const PORT = 7069;
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
